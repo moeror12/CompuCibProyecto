5cee52fab340abef3549fbd152e7371b07b3267b
moeror12 <129572875+moeror12@users.noreply.github.com>
2023-05-23 20:36:18 -0500

    Add files via upload

---
+++ Tiendita/Tiendita/IServices/IProductoService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tiendita.Models;
+
+namespace Tiendita.IServices
+{
+    public interface IProductoService
+    {
+        public List<Producto> ListarProducto();
+        public Producto ObtenerProducto(int productoId);
+        public int RegistrarProducto(Producto p);
+        public int ActualizarProducto(Producto p);
+        public int EliminarProducto(int pId);
+        public List<Categoria> ListarCategorias();
+        
+    }
+}
